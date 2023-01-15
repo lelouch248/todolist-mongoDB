b8f0a313dacfea724563cffa80dd42bfc005724a
lelouch248 <lelouch248@users.noreply.github.com>
2023-01-15 15:46:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+var items = ['buy food','cook food','eat food'];
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+  
+app.get("/", function (req, res) {
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US",options);
+  res.render("list", { kindOfDay: day , newListItem:items});
+});
+
+
+app.post("/", function (req, res) {
+  items.push(req.body.newItem);
+  res.redirect("/");
+});
+
+
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
+});
+
+
+
