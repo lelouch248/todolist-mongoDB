4e0fd2327ff173d15a429283b83af1e9b3ad902b
lelouch248 <lelouch248@users.noreply.github.com>
2023-01-16 20:24:31 +0530

    added custom module for the day

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const ejs = require("ejs");
+
 const app = express();
 
-var items = ["buy food", "cook food"];
-var workItems = [];
+const items = ["buy food", "cook food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-US", options);
-
+  
+  let day = date.getDay();
   res.render("list", { ListTitle: day, newListItem: items });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.newItem;
-  console.log(req.body);
-  console.log(req.body.list); 
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
